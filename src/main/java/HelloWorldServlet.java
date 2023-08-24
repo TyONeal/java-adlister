2919193c82d3880beb0997f036e4651626c3bf9c
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-08-24 09:30:56 -0500

    finished servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,35 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.Objects;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+
+   long pageCounter = 0;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        response.setContentType("text/html");
+
+        String searchName = request.getParameter("name");
+        String resetCounter = request.getParameter("reset");
+
+        if(searchName != null && !searchName.isEmpty()) {
+            out.println("<h1>Hello, " + searchName + "! " + pageCounter +  " have visited this page...</h1>");
+        } else {
+            out.println("<h1>Hello, World!</h1>");
+        }
+
+        pageCounter++;
+
+        if(resetCounter == "reset") {
+            pageCounter = 0;
+        }
+
 
     }
 }
