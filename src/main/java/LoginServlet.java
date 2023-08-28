f560e90668043baad9eff18b008508d23e5c749f
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-08-27 22:45:02 -0500

    finished sessions and cookies exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,15 +8,24 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        Boolean validAttempt = (Boolean) request.getSession().getAttribute("validAttempt");
+        if(validAttempt != null && validAttempt) {
+            response.sendRedirect("/profile");
+            return;
+        }
+
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
+        request.setAttribute("username", username);
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
 
         if (validAttempt) {
+            request.getSession().setAttribute("validAttempt", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet ("/logout")
+public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(!((boolean) request.getSession().getAttribute("validAttempt"))) {
+        Boolean isLoggedIn = (Boolean) request.getSession().getAttribute("validAttempt");
+        if(isLoggedIn != null && isLoggedIn) {
+            request.getSession().invalidate();
             response.sendRedirect("/login");
-            return;
         }
-        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
