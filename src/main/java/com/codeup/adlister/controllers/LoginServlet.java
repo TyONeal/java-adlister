fa7a0c7177801bde330dd80cffd24bc88f987e24
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-08-30 10:21:42 -0500

    finished passwords exercise

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,14 +24,16 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        String hash = BCrypt.hashpw(password, BCrypt.gensalt());
         User user = DaoFactory.getUsersDao().findByUsername(username);
 
         if (user == null) {
             response.sendRedirect("/login");
             return;
         }
-
-        boolean validAttempt = password.equals(user.getPassword());
+        System.out.println(password);
+        System.out.println(user.getPassword());
+        boolean validAttempt = BCrypt.checkpw(password, hash);
 
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
